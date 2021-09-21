a1986ed415e29034fe691d4792064ee615c32d9c
silvaisabela <eu.silvaisabela@gmail.com>
2021-09-21 20:46:36 -0300

    feat: create default theme file

---
+++ src/styles/theme.style.js
@@ -0,0 +1,17 @@
+export default {
+    fontWeight: {
+        light: "300",
+        regular: "400",
+        bold: "700"
+    },
+    fontColor: {
+        primary: "#231F20",
+        invert: "#FFFFFF",
+    },
+    color: {
+        primary: '#2DC1A6',
+        secondary: '#7963FF',
+        tertiary: "#F4F4F4",
+        error: "#DC3838"
+    }
+};
