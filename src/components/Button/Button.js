9cf9c30aeef47d30b7f18a651e51e65f17ac9ba5
silvaisabela <eu.silvaisabela@gmail.com>
2021-09-22 20:11:48 -0300

    fix: add propType plus to boolean

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,30 +1,31 @@
-import React from 'react';
+import React from 'react'
 import { func } from 'prop-types'
-import { Text, Pressable, View } from 'react-native';
-import { ButtonStyles } from './Button.style';
-import { ArrowLeft, Plus } from '../../../assets/Icons';
+import { Text, Pressable, View } from 'react-native'
+import { ButtonStyles } from './Button.style'
+import { ArrowLeft, Plus } from '../../../assets/Icons'
 
 const propTypes = {
-    onPress: func.isRequired,
-    text: String,
-    type: String,
+  onPress: func.isRequired,
+  text: String,
+  type: String,
+  plus: Boolean
 }
 
 const buttonThemes = {
-    "primary": ButtonStyles.button,
-    "secondary": ButtonStyles.buttonSecondary,
-    "tertiary": ButtonStyles.buttonTertiary
+  primary: ButtonStyles.button,
+  secondary: ButtonStyles.buttonSecondary,
+  tertiary: ButtonStyles.buttonTertiary
 }
 
-const Button = ({ onPress, text, plus, type = "primary", ...props }) => (
-    <Pressable style={buttonThemes[type]} onPress={onPress} {...props}>
-        <View style={ButtonStyles.container} >
-            <Text style={ButtonStyles.text}>{text}</Text>
-            {plus ? <Plus /> : <ArrowLeft aria-hidden="true" />}
-        </View>
-    </Pressable>
+const Button = ({ onPress, text, plus, type = 'primary', ...props }) => (
+  <Pressable style={buttonThemes[type]} onPress={onPress} {...props}>
+    <View style={ButtonStyles.container} >
+      <Text style={ButtonStyles.text}>{text}</Text>
+      {plus ? <Plus /> : <ArrowLeft aria-hidden="true" />}
+    </View>
+  </Pressable>
 )
 
 Button.propTypes = propTypes
 
-export { Button };
+export { Button }
