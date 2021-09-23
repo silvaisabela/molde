89bfef5a192e989220a7247c6b68cd4a108cc7e0
silvaisabela <eu.silvaisabela@gmail.com>
2021-09-23 18:23:31 -0300

    feat: add button

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, View } from 'react-native'
+import { Button } from './src/components/Button/Button'
 
 export default function App () {
   return (
     <View style={styles.container}>
+      <Button />
       <StatusBar style="auto" />
     </View >
   )
