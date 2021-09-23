416ed8c92715ce782cb491dd3fb53299e20ecbe8
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2021-09-23 17:03:28 -0300

    add text input icons

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import { Text, Pressable, View, TextInput } from 'react-native';
-import { InputTextStyles } from './InputText.style';
-import { Plus } from '../../../assets/Icons';
+import React from 'react'
+import { Pressable, View, TextInput } from 'react-native'
+import { InputTextStyles } from './InputText.style'
+import { func } from 'prop-types'
+import { Plus } from '../../../assets/Icons'
+import { Check } from '../../../assets/Icons/Check'
+import Uncheck from '../../../assets/Icons/Uncheck'
 
+const propTypes = {
+  onPress: func,
+  text: String,
+  plus: Boolean,
+  checked: Boolean,
+  noIcon: Boolean
+}
 
-const InputText = ({text, plus, onPress}) =>(
+const InputText = ({ text = 'Digite aqui', plus, onPress, checked, noIcon }) => (
     <Pressable>
         <View style={InputTextStyles.container}>
             <TextInput
             placeholder={text}
             style={InputTextStyles.inputText}
-            onChange={()=> console.warn("Aqui")}
+            /* onChange={} */
             />
-            {plus ?
-            <Pressable style={InputTextStyles.icon} onPress={onPress}>
+            {noIcon
+              ? null
+              : plus
+                ? <Pressable style={InputTextStyles.icon} onPress={onPress}>
                 <Plus/>
             </Pressable>
-            : null}
+                : checked ? <Check style={InputTextStyles.icon}/> : <Uncheck style={InputTextStyles.icon}/>}
+
         </View>
     </Pressable>
 )
 
-export {InputText};
+InputText.propTypes = propTypes
+
+export { InputText }
