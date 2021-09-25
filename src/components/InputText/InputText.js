780056d2a8fd55d47e00b7c727a17afcfe8e1055
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2021-09-25 12:45:26 -0300

    fix: text and icons position

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { func, String, Boolean } from 'prop-types'
-import { Pressable, View, TextInput } from 'react-native'
+import { Pressable, TextInput } from 'react-native'
 import { InputTextStyles } from './InputText.style'
 import { Check, Uncheck, Plus } from '../../../assets/Icons'
 import theme from '../../styles/theme.style'
@@ -15,22 +15,19 @@ const propTypes = {
 }
 
 const InputText = ({ text = 'Digite aqui', plus, onPress, check, noIcon, onChange }) => (
-    <Pressable>
-        <View style={InputTextStyles.container}>
+    <Pressable style={InputTextStyles.container}>
             <TextInput
             placeholder={text}
-            style={InputTextStyles.inputText}
             onChange={onChange}
+            style={InputTextStyles.textInput}
             />
             {noIcon
               ? null
               : plus
-                ? <Pressable style={InputTextStyles.icon} onPress={onPress}>
+                ? <Pressable onPress={onPress}>
                 <Plus color={theme.color.secondary}/>
             </Pressable>
-                : check ? <Check style={InputTextStyles.icon}/> : <Uncheck style={InputTextStyles.icon}/>}
-
-        </View>
+                : check ? <Check/> : <Uncheck/>}
     </Pressable>
 )
 
