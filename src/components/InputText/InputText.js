3d874d21bd4656a47add3973411ddc5bccc20a72
silvaisabela <eu.silvaisabela@gmail.com>
2021-09-26 23:26:44 -0300

    fix: validation componente

--- src/components/InputText/InputText.js
+++ src/components/InputText/InputText.js
@@ -1,40 +1,82 @@
 import React from 'react'
-import { func, String, Boolean } from 'prop-types'
-import { Pressable, TextInput, View, Text } from 'react-native'
+import { func, string, boolean } from 'prop-types'
+import { Pressable, TextInput as Input, View, Text } from 'react-native'
 import { InputTextStyles } from './InputText.style'
 import { Check, Uncheck, Plus } from '../../../assets/Icons'
 import theme from '../../styles/theme.style'
 
 const propTypes = {
-  onPress: func,
-  text: String,
-  plus: Boolean,
-  check: Boolean,
-  noIcon: Boolean,
-  messageError: String,
-  messageDefault: String,
-  onChange: func
+  onChange: func,
+  value: string,
+  placeholder: string,
+  plus: boolean,
+  valid: boolean,
+  helperText: string,
+  errorMessage: string,
+  onClickPlus: func
 }
 
-const InputText = ({ text = 'Digite aqui', plus, onPress, check, noIcon, onChange, messageError, messageDefault }) => (
-    <Pressable >
-      <View style={InputTextStyles.container}>
-            <TextInput
-            placeholder={text}
-            onChange={onChange}
-            style={InputTextStyles.textInput}
-            />
-            {noIcon
-              ? null
-              : plus
-                ? <Pressable onPress={onPress}>
-                <Plus color={theme.color.secondary}/>
-            </Pressable>
-                : check ? <Check/> : <Uncheck/>}
+const InputIcon = (plus, errorMessage, valid, onClick) => {
+  if (plus) {
+    return (
+      <Pressable onPress={onClick} style={InputTextStyles.baseIcon}>
+        <Plus color={theme.color.secondary} />
+      </Pressable>
+    )
+  }
+
+  if (errorMessage) {
+    return (
+      <View>
+        <Uncheck style={InputTextStyles.baseIcon} />
       </View>
-      {messageDefault ? <Text style={InputTextStyles.default}>{messageDefault}</Text> : null}
-      {messageError ? <Text style={InputTextStyles.error}>{messageError}</Text> : null}
-    </Pressable>
+    )
+  }
+
+  if (valid) {
+    return (
+      <View>
+        <Check style={InputTextStyles.baseIcon} />
+      </View>
+    )
+  }
+
+  return <></>
+}
+
+const InputMessage = (errorMessage, helperText) => {
+  if (errorMessage) {
+    return <Text style={InputTextStyles.error}>{errorMessage}</Text>
+  }
+
+  return <Text style={InputTextStyles.default}>{helperText}</Text>
+}
+
+const InputText = ({
+  placeholder,
+  plus,
+  valid,
+  helperText,
+  errorMessage,
+  onClickPlus,
+  ...props
+}) => (
+  <Pressable>
+    <View style={InputTextStyles.container}>
+      <View style={InputTextStyles.containerInput}>
+        <Input
+          style={InputTextStyles.input}
+          placeholder={placeholder}
+          {...props}
+        />
+        {InputIcon(plus, errorMessage, valid, onClickPlus)}
+      </View>
+    </View>
+
+    {(helperText || errorMessage) && (
+      <View>{InputMessage(errorMessage, helperText)}</View>
+    )}
+  </Pressable>
 )
 
 InputText.propTypes = propTypes
