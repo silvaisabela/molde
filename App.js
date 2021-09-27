959113c3cfff627c836b88574ec9b4deceb2b0f7
silvaisabela <eu.silvaisabela@gmail.com>
2021-09-26 23:29:27 -0300

    style:  lint fix

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
-import { InputText } from './src/components/InputText/InputText'
 
 export default function App () {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <InputText />
     </View >
   )
 }
