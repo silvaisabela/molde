a3f82e15a8477d3924cba86f72909fecce39944a
renatoloren <renatoloren226@gmail.com>
2021-09-29 02:00:01 -0300

    fix: add BackButton folder

---
+++ src/components/BackButton/BackButton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { func, String, Boolean } from 'prop-types'
+import { Pressable, View } from 'react-native'
+import { BackButtonStyles } from './BackButton.style'
+import { Back, Close } from '../../../assets/Icons'
+
+const propTypes = {
+  onPress: func,
+  type: String,
+}
+
+const BackButton = ({ onPress, type, ...props }) => (
+  <Pressable style={BackButtonStyles.button} onPress={onPress} {...props}>
+    <View style={BackButtonStyles.container} >
+      {(type == 'close') ? <Close /> : <Back />}
+    </View>
+  </Pressable>
+)
+
+BackButton.propTypes = propTypes
+
+export { BackButton }
---
+++ src/components/BackButton/BackButton.style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+const BackButtonStyles = StyleSheet.create({
+  button: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+  },
+  container: {
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'center'
+  }
+})
+
+export { BackButtonStyles }
