f23a97168c62fe72bf53245cc56e451bf267ef53
renatoloren <renatoloren226@gmail.com>
2021-09-29 01:55:13 -0300

    feat: create component BackButton

---
+++ assets/Icons/Close.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { Number, String } from 'prop-types'
+import Svg, { Path } from 'react-native-svg'
+
+const propTypes = {
+  width: Number,
+  height: Number,
+  color: String
+}
+
+const Close = ({ width = 40, height = 40, color = '#231F20', ...props }) => {
+  return (
+    <Svg
+      width={width}
+      height={height}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Path
+        stroke={color}
+        strokeWidth={3}
+        strokeLinecap="round"
+        d="M10.934 28.819l18.585-17.144M28.923 28.937L11.779 10.352"
+      />
+    </Svg>
+  );
+}
+
+Close.propTypes = propTypes
+
+export { Close };
+
--- assets/Icons/index.js
+++ assets/Icons/index.js
@@ -1,2 +1,4 @@
 export * from './ArrowLeft'
 export * from './Plus'
+export * from './Close'
+export * from './Back'
