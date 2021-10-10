bcaad3f86eee94e592b3866b02de19887f13a9e1
silvaisabela <eu.silvaisabela@gmail.com>
2021-10-09 23:48:19 -0300

    feat: add page signin to app

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
 import React from 'react'
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
+import { Signin } from './src/pages/Signin/Signin'
 
 export default function App () {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-    </View >
+      <Signin />
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
