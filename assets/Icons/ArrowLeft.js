70934f59e7d30e6e64d528e5c6236829c4da437e
silvaisabela <eu.silvaisabela@gmail.com>
2021-10-09 22:06:32 -0300

    fix: change propTypes

--- assets/Icons/ArrowLeft.js
+++ assets/Icons/ArrowLeft.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import { Number, String } from 'prop-types'
+import { number, string } from 'prop-types'
 import Svg, { Path } from 'react-native-svg'
 
 const propTypes = {
-  width: Number,
-  height: Number,
-  color: String
+  width: number,
+  height: number,
+  color: string
 }
 
 const ArrowLeft = ({ width = 29, height = 16, color = '#231F20', ...props }) => {
--- assets/Icons/Close.js
+++ assets/Icons/Close.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import { Number, String } from 'prop-types'
+import { number, string } from 'prop-types'
 import Svg, { Path } from 'react-native-svg'
 
 const propTypes = {
-  width: Number,
-  height: Number,
-  color: String
+  width: number,
+  height: number,
+  color: string
 }
 
 const Close = ({ width = 40, height = 40, color = '#231F20', ...props }) => {
