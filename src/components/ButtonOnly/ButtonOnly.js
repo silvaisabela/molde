98cdbc959b61be4add2aff22300e8fe68259b667
silvaisabela <eu.silvaisabela@gmail.com>
2021-10-09 22:10:29 -0300

    fix: change arrow icon and remove width from style

--- src/components/ButtonOnly/ButtonOnly.js
+++ src/components/ButtonOnly/ButtonOnly.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { func, string } from 'prop-types'
 import { Pressable, View } from 'react-native'
 import { ButtonOnlyStyles } from './ButtonOnly.style'
-import { ArrowRight, Close } from '../../../assets/Icons'
+import { ArrowLeft, Close } from '../../../assets/Icons'
 
 const propTypes = {
   onPress: func,
@@ -12,7 +12,7 @@ const propTypes = {
 const ButtonOnly = ({ onPress, type, ...props }) => (
   <Pressable style={ButtonOnlyStyles.button} onPress={onPress} {...props}>
     <View style={ButtonOnlyStyles.container} >
-      {(type === 'close') ? <Close /> : <ArrowRight />}
+      {(type === 'close') ? <Close /> : <ArrowLeft/>}
     </View>
   </Pressable>
 )
