013c38a54fd1c31c1bc1b313ceda3ec392b65380
silvaisabela <eu.silvaisabela@gmail.com>
2021-10-09 23:47:06 -0300

    feat: create login page

---
+++ src/pages/Signin/Signin.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { View } from 'react-native'
+import { ArrowRight, Logo } from '../../../assets/Icons'
+import { SigninStyles } from './signin.styles'
+import { Title } from '../../components/Title'
+import { InputText } from '../../components/InputText'
+import { InputPassword } from '../../components/InputPassword'
+import { Link } from '../../components/Link'
+import { Button } from '../../components/Button'
+import { LeftType } from '../../components/AppBar'
+import { SigninLayout } from '../../components/SigninLayout/SigninLayout'
+
+const Signin = () => {
+  return (
+    <SigninLayout
+      leftType={LeftType.Back}
+      onLeftClick={() => {}}
+      rightIcon={<Logo />}
+    >
+      <View style={SigninStyles.containerTitle}>
+        <Title weight="bold" size="6xl">
+          Molde a sua forma de escutar o mundo.
+        </Title>
+        <View style={SigninStyles.titleDecoration} />
+      </View>
+      <View style={SigninStyles.containerInput}>
+        <InputText style={SigninStyles.input} placeholder="Email" />
+        <InputPassword placeholder="senha" />
+      </View>
+      <Link style={SigninStyles.link}>Esqueci minha senha</Link>
+      <View style={SigninStyles.containerButton}>
+        <Button type="secondary" icon={<ArrowRight />} large>
+          Entrar
+        </Button>
+      </View>
+    </SigninLayout>
+  )
+}
+
+export { Signin }
---
+++ src/pages/Signin/signin.styles.js
@@ -0,0 +1,75 @@
+import { StyleSheet, Dimensions, Platform } from 'react-native'
+import theme from '../../styles/theme.style'
+
+const window = Dimensions.get('window')
+
+const titleDecorationTop = window.width <= 375 ? 104 : 85
+
+const innerContainerHeight = Platform.OS === 'ios'
+  ? window.height - (window.height / 4)
+  : window.height - (window.height / 5)
+
+const SigninStyles = StyleSheet.create({
+  safe: {
+    backgroundColor: theme.backgroungColor.primary,
+    flex: 1
+  },
+  keyboard: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  backgroundIcon: {
+    zIndex: 1,
+    width: window.width,
+    position: 'absolute',
+    top: 0
+  },
+  innerContainer: {
+    backgroundColor: theme.backgroungColor.secondary,
+    width: window.width,
+    height: innerContainerHeight,
+    borderWidth: 0,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    padding: 30,
+    zIndex: 2
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  containerTitle: {
+    width: 220,
+    marginTop: 50
+  },
+  containerInput: {
+    marginTop: 42,
+    height: 120,
+    justifyContent: 'space-between'
+  },
+  link: {
+    alignItems: 'center',
+    marginTop: 10
+  },
+  buttonContainer: {
+    justifyContent: 'space-between'
+  },
+  containerButton: {
+    marginTop: 40
+  },
+  titleDecoration: {
+    position: 'absolute',
+    backgroundColor: theme.color.primary,
+    width: 200,
+    height: 10,
+    left: 0,
+    top: titleDecorationTop,
+    zIndex: -1
+  }
+})
+
+export { SigninStyles }
