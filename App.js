6a069b132785185586ec87ebe55acadfafae98ca
silvaisabela <eu.silvaisabela@gmail.com>
2021-10-10 01:23:39 -0300

    feat: add font family to app file

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import * as React from 'react'
+import { useFonts } from 'expo-font'
 import { Signin } from './src/pages/Signin/Signin'
 
 export default function App () {
+  const [loaded] = useFonts({
+    Roboto: require('./assets/Fonts/Roboto-Regular.ttf'),
+    RobotoBold: require('./assets/Fonts/Roboto-Bold.ttf'),
+    RobotoLight: require('./assets/Fonts/Roboto-Light.ttf')
+  })
+
+  if (!loaded) {
+    return null
+  }
   return (
       <Signin />
   )
