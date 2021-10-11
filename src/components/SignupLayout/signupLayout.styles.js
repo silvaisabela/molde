8778746787b9a9715c92e19020aa74f9efcbce43
silvaisabela <eu.silvaisabela@gmail.com>
2021-10-11 01:21:55 -0300

    feat: create component signup layout

---
+++ src/components/SignupLayout/signupLayout.styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+const SignupLayoutStyles = StyleSheet.create({
+  safe: {
+    flex: 1
+  },
+  keyboard: {
+    flex: 1
+  },
+  scroll: {
+    flexGrow: 1
+  },
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  body: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
+
+export { SignupLayoutStyles }
