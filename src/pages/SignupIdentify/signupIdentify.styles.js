b89720d3d804aa19b41f02bd2f9eb83e47e82e99
silvaisabela <eu.silvaisabela@gmail.com>
2021-10-11 01:23:03 -0300

    feat: create user identification step

---
+++ src/pages/SignupIdentify/signupIdentify.styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+const SignupIdentifyStyles = StyleSheet.create({
+  containerTitle: {
+    width: 270
+  },
+  containerInput: {
+    marginTop: 40
+  },
+  containerButton: {
+    alignSelf: 'flex-end'
+  }
+})
+
+export { SignupIdentifyStyles }
