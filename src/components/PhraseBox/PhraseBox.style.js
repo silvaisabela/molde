e3c2e4dbee8787d74d861f86b6f5ffc08eae1bda
renatoloren <renatoloren226@gmail.com>
2021-10-12 00:38:38 -0300

    feat: create component PhraseBox

---
+++ src/components/PhraseBox/PhraseBox.style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+import theme from '../../styles/theme.style'
+
+const PhraseBoxStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    alignSelf:"flex-start",
+    height: 30,
+    padding: 12,
+    borderWidth: 1,
+    borderColor: theme.backgroungColor.primary,
+    borderRadius: 50,
+  },
+  text: {
+    fontSize: theme.fontSize.md,
+    letterSpacing: theme.letterSpacing.sm,
+    fontWeight: theme.fontWeight.regular,
+    color: theme.fontColor.primary,
+    marginRight: 5
+  }
+})
+
+export { PhraseBoxStyles }
